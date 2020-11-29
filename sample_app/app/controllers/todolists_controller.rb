15ae2042d9ef25effbcbdef1775a15f269eef25f
EC2 Default User <ec2-user@ip-172-31-28-253.ec2.internal>
2020-11-29 08:32:20 +0000

    [Add] ライブラリ:[Rubocop]

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -4,7 +4,7 @@ class TodolistsController < ApplicationController
     @list = List.new
   end
 
-    # 以下を追加
+  # 以下を追加
   def create
     # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -12,9 +12,6 @@ class TodolistsController < ApplicationController
     list.save
     # 詳細画面へリダイレクト
     redirect_to todolist_path(list.id)
-  end
-
-  def index
   end
 
   def index
@@ -36,12 +33,13 @@ class TodolistsController < ApplicationController
   end
 
   def destroy
-    list = List.find(params[:id])  # データ（レコード）を1件取得
-    list.destroy  # データ（レコード）を削除
-    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+    list = List.find(params[:id]) # データ（レコード）を1件取得
+    list.destroy # データ（レコード）を削除
+    redirect_to todolists_path # 投稿一覧画面へリダイレクト
   end
 
   private
+
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body, :image)
